555c526087c6e7d5d5bedce4e8a4a1447cd2b23b
wzc <forgotten@outlook.com>
2015-04-10 11:04:58 +0800

    add ch3.1

---
+++ ch3.1.scm
@@ -0,0 +1,8 @@
+(define balance 100)
+(define (withdraw amount)
+  (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+	     balance)
+      "Insufficient funds"))
+(withdraw 25)
+	      
